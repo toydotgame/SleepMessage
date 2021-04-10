f05565206cc7da67c54fbb4357fc2f9dc233f1b8
toydotgame <toydotgame@gmail.com>
2021-04-10 19:44:46 +1000

    Added sleep detector

--- src/io/github/toydotgame/SleepListener.java
+++ src/io/github/toydotgame/SleepListener.java
@@ -1,7 +1,16 @@
 package io.github.toydotgame;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
 
 public class SleepListener implements Listener {
-
+	@EventHandler
+	public void onPlayerSleep(PlayerBedEnterEvent bedEnterEvent) {
+		Player player = bedEnterEvent.getPlayer();
+		String playerNameString = player.getName();
+		Bukkit.broadcastMessage(playerNameString + " slept in a bed.");
+	}
 }
