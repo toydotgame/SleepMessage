ed423717b0afe25d74f963207bf0676baa29b2e0
toydotgame <toydotgame@gmail.com>
2021-04-10 19:31:54 +1000

    Initialised listener class Initialised with a S, not a Z.

--- src/io/github/toydotgame/Main.java
+++ src/io/github/toydotgame/Main.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {	
 	@Override
 	public void onEnable() {
+		getServer().getPluginManager().registerEvents(new SleepListener(), this);
 		System.out.print("[Sleep Messages] Plugin loaded successfully!");
 	}
 }
---
+++ src/io/github/toydotgame/SleepListener.java
@@ -0,0 +1,7 @@
+package io.github.toydotgame;
+
+import org.bukkit.event.Listener;
+
+public class SleepListener implements Listener {
+
+}
